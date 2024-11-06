7e0fe29184950b3bd0f9fc1e124744002cd9937f
VaTimirshin <vadimyxll@gmail.com>
2024-11-06 14:56:48 +0300

    Adds file

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=100)
+    task = models.TextField('Описание')
+    link_proj = models.TextField('Проект')
+    link_author = models.TextField('Автор')
+    status = models.TextField('Статус')
+    priority = models.TextField('Приоритет')
+    date_create = models.TextField('Дата создания')
+    date_update = models.TextField('Дата обновления')
+    period_execution = models.TextField('Срок выполнения')
+    tester = models.TextField('Ответсвенный за тестировку')
+
+    def __str__(self):
+        return self.title
