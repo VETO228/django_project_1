dc4a50644bb0f2360613e500e09c83bc274b236a
Vadim <vadimyxll@gmail.com>
2024-11-14 00:46:58 +0300

    Adds file

--- myproject/main/models.py
+++ myproject/main/models.py
@@ -1,17 +1,20 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+
+from .base.services import get_path_upload_avatar, validate_size_image
 
 
 class Task(models.Model):
     title = models.CharField('Название', max_length=100)
     task = models.TextField('Описание')
-    link_proj = models.TextField('Проект')
-    link_author = models.TextField('Автор')
-    status = models.TextField('Статус')
-    priority = models.TextField('Приоритет')
+    link_proj = models.FileField('Проект')
+    link_author = models.CharField('Автор', max_length=100)
+    status = models.CharField('Статус', max_length=100)
+    priority = models.CharField('Приоритет', max_length=100, blank=True, null=True)
     date_create = models.DateTimeField('Дата создания', auto_now_add=True)
     date_update = models.DateTimeField('Дата обновления', auto_now=True)
-    period_execution = models.TextField('Срок выполнения')
-    tester = models.TextField('Ответсвенный за тестировку')
+    period_execution = models.DateTimeField('Срок выполнения', blank=True, null=True)
+    tester = models.CharField('Ответственный за тестировку', max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.title
@@ -21,14 +24,46 @@ class Task(models.Model):
 
 
 class Register(models.Model):
-    name = models.CharField('Имя пользователя', max_length=100)
-    surname = models.CharField('Фамилия пользователя', max_length=100)
-    avatar = models.ImageField()
-    character = models.TextField('Роль на платформе')
-    project = models.ImageField()
+    """Модель пользователя"""
+    name = models.CharField('Имя пользователя', max_length=100, blank=True, null=True)
+    surname = models.CharField('Фамилия пользователя', max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=100, unique=True)
+    avatar = models.ImageField(
+        upload_to=get_path_upload_avatar,
+        blank=True,
+        null=True,
+        validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'gif']), validate_size_image])
+    character = models.CharField('Роль на платформе', max_length=100, blank=True, null=True)
+    project = models.FileField(blank=True, null=True)
+
+
+    @property
+    def is_authenticated(self):
+        """Всегда возращает True. Это способ узнать, был ли пользователь аутентифицированы
+        """
+        return True
+
 
     def __str__(self):
         return self.name
 
+
     class Meta:
         pass
+
+
+class Projects(models.Model):
+    name = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание')
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.CharField('Статус', max_length=100)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        pass
+
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .serializer import TaskSerializer, RegSerializer
-from .models import Task
+from .serializer import TaskSerializer, RegSerializer, ProjectSerializer
+from .models import Task, Register
+from rest_framework.views import APIView
 
 
 def index(request):
     return render(request, 'main/index.html')
 
+
 def about(request):
     return render(request, 'main/about.html')
 
 
-class TaskAPIView(generics.ListAPIView):
+class TaskAPIView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-class RegisterAPIView(generics.CreateAPIView):
-    queryset = Task.objects.all()
+
+class RegisterAPIView(generics.ListCreateAPIView):
+    queryset = Register.objects.all()
     serializer_class = RegSerializer
 
+
+class ProjectsAPIView(APIView):
+    def get(self, request):
+        return Response({'title': 'Projects'})
